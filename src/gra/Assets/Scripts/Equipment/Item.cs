41eaefaabf938fc14bf1a612f396990607f2a024
Dominik Magiera <domino1162@gmail.com>
2015-05-03 13:18:34 +0200

    ok

---
+++ src/gra/Assets/Scripts/Equipment/Item.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[System.Serializable]
+public class Item  {
+	public string itemName;
+	public int itemID;
+	public string itemDescription;
+	public Texture2D itemIcon;
+
+	public ItemType itemType;
+
+	public enum ItemType {
+		Weapon,
+		VaccinePart,
+		Ammo
+	}
+
+	public Item(string name, int ID, string description, ItemType type){
+		itemName = name;
+		itemID = ID;
+		itemIcon = Resources.Load<Texture2D> ("Item Icons/"+ name);
+		itemDescription = description;
+		itemType = type;
+	}
+	public Item(){
+
+	}
+
+}
---
+++ src/gra/Assets/Scripts/Equipment/ItemDatabase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour {
+	public List<Item> items = new List<Item> ();
+
+
+	void Start(){
+		items.Add (new Item ("2", 0, "lol", Item.ItemType.Weapon));
+		items.Add (new Item ("3", 1, "lol1", Item.ItemType.Weapon));
+	}
+
+
+}
