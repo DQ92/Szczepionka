fc169e00a062748b9918f0318d9f255c0ab6eb28
Kuta Daniel <danielq44@gmail.com>
2015-05-04 19:23:26 +0200

    new

---
+++ src/gra/Assets/Scenes/Menu/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public GUITexture background;
+	public GUIStyle titleStyle;
+	public GUIStyle buttonStyle;
+	
+	int buttonWidth = 200;
+	int buttonHeight = 30;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI()
+	{
+		background.pixelInset = new Rect(-Screen.width/2,-Screen.height/2,Screen.width,Screen.height);
+
+		GUI.Label(new Rect(50,50,Screen.width,70),"Szczepionka",titleStyle);
+
+		int xPos = 150;
+		if (GUI.Button(new Rect(Screen.width - buttonWidth - xPos ,
+		                        Screen.height - buttonHeight - 250, buttonWidth, buttonHeight),
+		               			"Nowa Gra",buttonStyle))
+		{
+			Application.LoadLevel("TestLevel");
+		}
+
+		if (GUI.Button(new Rect(Screen.width - buttonWidth - xPos ,
+		                        Screen.height - buttonHeight - 200, buttonWidth, buttonHeight),
+		               "Wznów",buttonStyle))
+		{
+
+		}
+
+		if (GUI.Button(new Rect(Screen.width - buttonWidth - xPos ,
+		                        Screen.height - buttonHeight - 150, buttonWidth, buttonHeight),
+		               "Wyjdź",buttonStyle))
+		{
+			Application.Quit();
+		}
+	}
+}
---
+++ src/gra/Assets/Scripts/Equipment/Item.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[System.Serializable]
+public class Item  {
+	public string itemName;
+	public int itemPower;
+	public string itemDescription;
+	public Texture2D itemIcon;
+
+	public ItemType itemType;
+
+	public enum ItemType {
+		Weapon,
+		VaccinePart,
+		Ammo,
+		Medic
+	}
+
+	public Item(string name, int ID, string description, ItemType type,int power){
+		itemName = name;
+		itemPower = power;
+		itemIcon = Resources.Load<Texture2D> ("Item Icons/"+ name);
+		itemDescription = description;
+		itemType = type;
+	}
+	public Item(){
+
+	}
+
+	public int use(){
+		if (itemType == ItemType.Medic) {
+
+			return-1;
+		}
+		return 0;
+	}
+
+}
