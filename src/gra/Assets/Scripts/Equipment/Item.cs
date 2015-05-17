4d8e9157e24ed7450c124c1fa6d571fa3c6df279
Aleksander Furtak <Aleksander@furtak.pwsz>
2015-05-17 20:16:13 +0200

    łączenie

--- src/gra/Assets/Scripts/Equipment/Item.cs
+++ src/gra/Assets/Scripts/Equipment/Item.cs
@@ -22,7 +22,7 @@ public class Item  : MonoBehaviour{
 		Medic
 	}
 
-	public Item(string name, int ID, string description, ItemType type,int power,WeaponManager weaponManager){
+	public Item(string name, int ID, string description, ItemType type,int power,WeaponManager weaponManager=null){
 		itemName = name;
 		itemPower = power;
 		itemIcon = Resources.Load<Texture2D> ("Item Icons/"+ name);
@@ -36,8 +36,13 @@ public class Item  : MonoBehaviour{
 
 	public int use(){
 		if (itemType == ItemType.Medic) {
+			weaponManager.addAmmo("M9",50);
 
 			return 1;
+		}
+		if (itemType == ItemType.M9Ammo) {
+			weaponManager.addAmmo("M9",50);
+			return 0;
 		}
 		return 0;
 	}
