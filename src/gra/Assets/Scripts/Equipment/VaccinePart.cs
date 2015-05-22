2da2164acb10feed49d6adffb8422b909257f8bc
Aleksander Furtak <Aleksander@furtak.pwsz>
2015-05-22 22:59:53 +0200

    części szczepionki

---
+++ src/gra/Assets/Scripts/Equipment/VaccinePart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VaccinePart : MonoBehaviour {
+	public string name;
+	public string desc;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnTriggerEnter( Collider other) {
+		if (other.gameObject.tag == "Player" && name!="") {
+			
+			GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>().AddItem(new Item(name,0,desc+"\nNiezbędna część szczepionki potrzebna do misji.",Item.ItemType.VaccinePart,5));
+			Destroy(gameObject,0);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
